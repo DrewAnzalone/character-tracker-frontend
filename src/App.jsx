3e2a8a45e59bd89e3445d491df24978634d78e41
Michael McBride <michaelmcbride113@gmail.com>
2025-03-19 15:21:05 -0400

    Initial changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { Navigate, Routes, Route } from 'react-router';
+
+import * as equipService from './services/equipService.js'
 
 import NavBar from './components/NavBar/NavBar';
 import SignUpForm from './components/SignUpForm/SignUpForm';
 import SignInForm from './components/SignInForm/SignInForm';
 import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
+import EquipList from './components/EquipList/EquipList.jsx';
 
 import { UserContext } from './contexts/UserContext';
+
+
 
 const App = () => {
   const { user } = useContext(UserContext);
---
+++ src/components/EquipList/EquipList.jsx
@@ -0,0 +1,6 @@
+const EquipList = (props) => {
+    return <main>Equip List</main>;
+}
+
+
+export default EquipList;
