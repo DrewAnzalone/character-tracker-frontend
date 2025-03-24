0242019c440943b1f5694a8f4adf8084d4c3eda1
Michael McBride <michaelmcbride113@gmail.com>
2025-03-24 15:34:06 -0400

    Added css-modules folder and created css for the table

--- src/components/EquipList/EquipList.jsx
+++ src/components/EquipList/EquipList.jsx
@@ -1,16 +1,17 @@
 import { Link } from 'react-router';
+import styles from '/src/css-modules/table.module.css'
 
 const EquipList = (props) => {
   return (
     <>
     <li><Link to='/equips/new'>New Equip</Link></li>
-      <table>
+      <table className={styles.table}>
         <tbody>
           <tr>
             <th>Equip Name</th>
             <th>Equip Type</th>
             <th>Equip Stats</th>
-            
+            <th>Delete?</th>
           </tr>
           {props.equips.map(equip =>
               <tr key={equip._id}>
