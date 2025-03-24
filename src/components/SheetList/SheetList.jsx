5755963e542f440f66b732ce48fd31dedd2d03c6
Drew-A <drew.a.anzalone@gmail.com>
2025-03-24 16:45:20 -0700

    flex grid columns sheet list

--- src/components/SheetList/SheetList.jsx
+++ src/components/SheetList/SheetList.jsx
@@ -6,28 +6,33 @@ const SheetList = (props) => {
   return (
     <>
       <Link to='/sheets/new' onClick={() => props.handleSelect(null)}>Create Sheet</Link>
-      {props.sheets.map(sheet => {
-        const stats = { baseHP: sheet.baseHP, baseAtk: sheet.baseAtk, baseDef: sheet.baseDef, baseMagic: sheet.baseMagic }
-        sheet.equips.forEach(equip => {
-          stats[equip.statModify] += equip.statValue;
-        });
+      <div className={styles.sheetgrid}>
+        {props.sheets.map(sheet => {
+          const stats = { baseHP: sheet.baseHP, baseAtk: sheet.baseAtk, baseDef: sheet.baseDef, baseMagic: sheet.baseMagic }
+          sheet.equips.forEach(equip => {
+            stats[equip.statModify] += equip.statValue;
+          });
 
-        return <Link onClick={() => props.handleSelect(sheet)} key={sheet._id} to={`/sheets/${sheet._id}`}>
-          <div className={styles.div}>
+          return <Link
+            key={sheet._id}
+            className={styles.div}
+            onClick={() => props.handleSelect(sheet)}
+            to={`/sheets/${sheet._id}`}
+          >
             <h3 className={styles.h3}>{sheet.name}</h3>
             <img src={images.Character} alt="" />
             <h3 className={styles.h3}>Level {sheet.level} {sheet.class}</h3>
-            <ul className={styles.ul} >
-              <li className={styles.li} >HP: {stats.baseHP}</li>
+            <ul className={styles.ul}>
+              <li className={styles.li}>HP: {stats.baseHP}</li>
               <li className={styles.li}>Atk: {stats.baseAtk}</li>
               <li className={styles.li}>Def: {stats.baseDef}</li>
               <li className={styles.li}>Magic: {stats.baseMagic}</li>
             </ul>
             <p className={styles.p}>Equipment: {sheet.equips.length}</p>
-          </div>
-        </Link>;
-      }
-      )}
+          </Link>;
+        }
+        )}
+      </div>
     </>
   )
 };
