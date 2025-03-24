2092763293eaacf8db2f6fe8445dd354bb1cf481
Drew-A <drew.a.anzalone@gmail.com>
2025-03-24 16:01:20 -0700

    default images on sheetlist

---
+++ src/assets/index.js
@@ -0,0 +1,18 @@
+import Armor from './Armor.png';
+import Axe from './Axe.png';
+import Bow from './Bow.png';
+import Character from './Character.png';
+import Dagger from './Dagger.png';
+import Flail from './Flail.png';
+import Hammer from './Hammer.png';
+import Mace from './Mace.png';
+import Nunchucks from './Nunchucks.png';
+import Shield from './Shield.png';
+import Shortsword from './Shortsword.png';
+import Spear from './Spear.png';
+import Staff from './Staff.png';
+import Sword from './Sword.png';
+import TwoHand from './Two-hand.png';
+import XBow from './XBow.png';
+
+export { Armor, Axe, Bow, Character, Dagger, Flail, Hammer, Mace, Nunchucks, Shield, Shortsword, Spear, Staff, Sword, TwoHand, XBow };
