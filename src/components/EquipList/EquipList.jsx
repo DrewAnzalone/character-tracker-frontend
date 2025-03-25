dc6829ae6a4806933d36d358885b97a5fcaf6132
Drew-A <drew.a.anzalone@gmail.com>
2025-03-24 18:04:37 -0700

    modal and some table css

--- src/components/EquipList/EquipList.jsx
+++ src/components/EquipList/EquipList.jsx
@@ -1,21 +1,42 @@
 import { Link } from 'react-router';
 import styles from '/src/components/EquipList/table.module.css'
+import * as images from '../../assets';
+
+const imageFiles = {
+  "Armor": images.Armor,
+  "Axe": images.Axe,
+  "Bow": images.Bow,
+  "Dagger": images.Dagger,
+  "Flail": images.Flail,
+  "Hammer": images.Hammer,
+  "Mace": images.Mace,
+  "Nunchucks": images.Nunchucks,
+  "Shield": images.Shield,
+  "Shortsword": images.Shortsword,
+  "Spear": images.Spear,
+  "Staff": images.Staff,
+  "Sword": images.Sword,
+  "Two-hand": images.TwoHand,
+  "XBow": images.XBow,
+};
 
 const EquipList = (props) => {
   return (
-    <>
+    <div className='margin'>
     <li><Link to='/equips/new'>New Equip</Link></li>
       <table className={styles.table}>
         <tbody>
           <tr>
+            <th></th>
             <th>Equip Name</th>
             <th>Equip Type</th>
             <th>Equip Stats</th>
             <th>Delete?</th>
           </tr>
           {props.equips.map(equip =>
-              <tr key={equip._id}>
-                <td onClick={() => props.handleSelect(equip)}>{equip.name}</td>
+              <tr key={equip._id} onClick={() => props.handleSelect(equip)}>
+                <td><img src={imageFiles[equip.type]} alt="Weapon glyph" /></td>
+                <td>{equip.name}</td>
                 <td>{equip.type}</td>
                 <td>{`+${equip.statValue} ${equip.statModify}`}</td>
                 <td><button onClick={() => props.handleDeleteEquip(equip._id)}>Delete</button></td>
@@ -23,7 +44,7 @@ const EquipList = (props) => {
           )}
         </tbody>
       </table>
-    </>
+    </div>
   );
 }
 
