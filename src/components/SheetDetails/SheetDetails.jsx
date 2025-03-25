1129fa6a4f5b8263dbaae675b656228fc0d5caf1
Drew-A <drew.a.anzalone@gmail.com>
2025-03-24 19:14:02 -0700

    left aligned button and added hover background color

--- src/components/SheetDetails/SheetDetails.jsx
+++ src/components/SheetDetails/SheetDetails.jsx
@@ -23,40 +23,41 @@ const SheetDetails = (props) => {
   } = props.sheet;
 
   return (
-    <div className={`margin ${styles.container}`}>
-      <button className={styles.button} onClick={() => navigate(-1)}>Back</button>
-      
-      <div className={styles.sheetCard}>
-        <h1 className={styles.h1}>{name}</h1>
-        <div className={styles.imgcontainer}>
-          <img src={images.Character} alt="Character placeholder" className={styles.img} />
-        </div>
-        <h2 className={styles.h2}>Level {level} {sheetClass}</h2>
-        <ul className={styles.statsList}>
-          <li className={styles.itemStat}>HP: {stats.baseHP}</li>
-          <li className={styles.itemStat}>Atk: {stats.baseAtk}</li>
-          <li className={styles.itemStat}>Def: {stats.baseDef}</li>
-          <li className={styles.itemStat}>Magic: {stats.baseMagic}</li>
-        </ul>
-      </div>
-  
-      <div>
-        {equips.map(equip =>
-          <div key={equip._id} className={styles.equipItem} onClick={() => props.handleSelect(equip)}>
-            <p>{equip.name}</p>
+    <>
+      <button className={`margin ${styles.button}`} onClick={() => navigate(-1)}>Back</button>
+      <div className={`margin ${styles.container}`}>
+        <div className={styles.sheetCard}>
+          <h1 className={styles.h1}>{name}</h1>
+          <div className={styles.imgcontainer}>
+            <img src={images.Character} alt="Character placeholder" className={styles.img} />
           </div>
-        )}
+          <h2 className={styles.h2}>Level {level} {sheetClass}</h2>
+          <ul className={styles.statsList}>
+            <li className={styles.itemStat}>HP: {stats.baseHP}</li>
+            <li className={styles.itemStat}>Atk: {stats.baseAtk}</li>
+            <li className={styles.itemStat}>Def: {stats.baseDef}</li>
+            <li className={styles.itemStat}>Magic: {stats.baseMagic}</li>
+          </ul>
+        </div>
+
+        <div>
+          {equips.map(equip =>
+            <div key={equip._id} className={styles.equipItem} onClick={() => props.handleSelect(equip)}>
+              <p>{equip.name}</p>
+            </div>
+          )}
+        </div>
+
+        <div>
+          <button className={styles.button}>
+            <Link to={`/sheets/${props.sheet._id}/edit`} className={styles.link}>Edit Sheet</Link>
+          </button>
+          <button className={styles.button} onClick={() => props.handleDeleteSheet(props.sheet._id)}>Delete</button>
+        </div>
       </div>
-  
-      <div>
-        <button className={styles.button}>
-          <Link to={`/sheets/${props.sheet._id}/edit`} className={styles.link}>Edit Sheet</Link>
-        </button>
-        <button className={styles.button} onClick={() => props.handleDeleteSheet(props.sheet._id)}>Delete</button>
-      </div>
-    </div>
+    </>
   );
-  
+
 }
 
 export default SheetDetails;
